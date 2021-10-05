4e6206383a1a3ca62d3a04f6e318231b5530d764
Lucas Santos Silva <luss1010@gmail.com>
2021-10-05 14:00:07 -0300

    Update tools_pay.py

--- tools_pay.py
+++ tools_pay.py
@@ -1,12 +1,12 @@
-'''import lnpay_py
+import lnpay_py
 from lnpay_py.wallet import LNPayWallet
 import pyqrcode
 import os
 
-# init lnpay
-lnpay_py.initialize(os.environ['lnpay_api_key'])
 
 def get_invoice(num_satoshis=2,memo='{02,13,22,44,55,59}, ex@gmail.com'):
+    # init lnpay
+    lnpay_py.initialize(os.environ['lnpay_api_key'])
     my_wallet = LNPayWallet(os.environ['wallet_id'])
     invoice_params = {
         'num_satoshis': num_satoshis,
@@ -17,14 +17,15 @@ def get_invoice(num_satoshis=2,memo='{02,13,22,44,55,59}, ex@gmail.com'):
     return invoice , myqr.png_as_base64_str(scale=6)
 
 def get_transactions():
+    # init lnpay
+    lnpay_py.initialize(os.environ['lnpay_api_key'])
     my_wallet = LNPayWallet(os.environ['wallet_invoice'])
     transactions = my_wallet.get_transactions()
     return transactions
 def get_info():
+    # init lnpay
+    lnpay_py.initialize(os.environ['lnpay_api_key'])
     my_wallet = LNPayWallet(os.environ['wallet_read'])
     info = my_wallet.get_info()
     return info
 
-if __name__=="__main__":
-    print(get_info())
-'''
