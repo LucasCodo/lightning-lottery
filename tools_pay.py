43ac6cec1fab86da2964041b9401509c1150ea5c
Lucas Santos Silva <luss1010@gmail.com>
2021-10-05 12:32:59 -0300

    Update tools_pay.py

--- tools_pay.py
+++ tools_pay.py
@@ -1,12 +1,13 @@
 import lnpay_py
 from lnpay_py.wallet import LNPayWallet
 import pyqrcode
+import os
 
 # init lnpay
-lnpay_py.initialize(lnpay_api_key)
+lnpay_py.initialize(os.environ['lnpay_api_key'])
 
-def get_invoice(num_satoshis=2,memo='{02,13,22,44,55,59}, luss1010@gmail.com'):
-    my_wallet = LNPayWallet(wallet_id)
+def get_invoice(num_satoshis=2,memo='{02,13,22,44,55,59}, ex@gmail.com'):
+    my_wallet = LNPayWallet(os.environ['wallet_id'])
     invoice_params = {
         'num_satoshis': num_satoshis,
         'memo': memo
@@ -16,11 +17,11 @@ def get_invoice(num_satoshis=2,memo='{02,13,22,44,55,59}, luss1010@gmail.com'):
     return invoice , myqr.png_as_base64_str(scale=6)
 
 def get_transactions():
-    my_wallet = LNPayWallet(wallet_invoice)
+    my_wallet = LNPayWallet(os.environ['wallet_invoice'])
     transactions = my_wallet.get_transactions()
     return transactions
 def get_info():
-    my_wallet = LNPayWallet(wallet_read)
+    my_wallet = LNPayWallet(os.environ['wallet_read'])
     info = my_wallet.get_info()
     return info
 
